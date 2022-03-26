7fd57274f3833dada9f9ab409875b20812f4aebb
Louis Delmas <louis.delmas@outlook.com>
2022-03-26 22:29:32 +0100

    Allow create set score only to admin user

--- backend/games/views/scores.py
+++ backend/games/views/scores.py
@@ -2,7 +2,7 @@ import django_filters
 from django.db.models import Sum
 from django.db.models.functions import Coalesce
 
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 from rest_framework import pagination, mixins, viewsets
 from authentication.models.user import User
 from games.serializers.scores import ScoreSerializer, LeaderboardSerializer
@@ -36,8 +36,17 @@ class ScoreViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     permission_classes = [IsAuthenticated]
     ordering = ["-when"]
 
+class ActionBasedPermission(AllowAny):
+    """
+    Grant or deny access to a view, based on a mapping in view.action_permissions
+    """
+    def has_permission(self, request, view):
+        for klass, actions in getattr(view, 'action_permissions', {}).items():
+            if view.action in actions:
+                return klass().has_permission(request, view)
+        return False
 
-class LeaderboardViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class LeaderboardViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = (
         Score.objects.values("user")
         .annotate(total_score=Coalesce(Sum("score"), 0))
@@ -46,4 +55,9 @@ class LeaderboardViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     filterset_class = ScoreFilter
     serializer_class = LeaderboardSerializer
     pagination_class = ScorePagination
-    permission_classes = [IsAuthenticated]
+
+    permission_classes = (ActionBasedPermission,)
+    action_permissions = {
+        IsAuthenticated: ['list'],
+        IsAdminUser: ['create']
+    }
